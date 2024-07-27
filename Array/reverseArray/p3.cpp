f047e6fe1adc8429f3b55c4742a42160874ec7ba
Purnamita Baral <purnamitabaral@gmail.com>
2024-07-28 00:52:26 +0530

    Second commit

---
+++ Array/reverseArray/p3.cpp
@@ -0,0 +1,23 @@
+// REVERSE ONLY LETTERS
+
+#include <string>
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int left = 0, right = s.size() - 1;
+    
+    while (left < right) {
+        if (!isalpha(s[left])) {
+            left++;
+        } else if (!isalpha(s[right])) {
+            right--;
+        } else {
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
+    }
+    
+    return s;
+    }
+};
