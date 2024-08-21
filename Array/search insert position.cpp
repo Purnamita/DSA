0725228cbf6da23238a46b1cd881048b3a638d5b
Purnamita Baral <purnamitabaral@gmail.com>
2024-08-21 15:19:01 +0530

    day7 commit

---
+++ Array/search insert position.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+#include <vector>
+#include <limits.h>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        int left = 0, right = n- 1;
+    
+        while (left <= right) {
+        int mid = left + (right - left) / 2;
+        
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    
+    // If the target is not found, return the position where it would be inserted
+    return left;
+        
+    }
+};
